c5861283ab115e8bf04979d0c63046d5f6b6c445
Steve Powell <steve.powell@edgewordstraining.co.uk>
2021-02-10 12:22:16 +0000

    Added a log message

--- GlobalParamDemo/Recording3.cs
+++ GlobalParamDemo/Recording3.cs
@@ -82,6 +82,8 @@ namespace GlobalParamDemo
             OpenBrowser();
             Delay.Milliseconds(0);
             
+            Report.Log(ReportLevel.Info, "User", "Blah", new RecordItemIndex(1));
+            
         }
 
 #region Image Feature Data
